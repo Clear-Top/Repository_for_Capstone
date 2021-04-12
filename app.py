cc5d312cc7980cf31e0c3bc30144557ee751be36
이재성 <raiclee@gmail.com>
2021-04-12 13:23:05 +0000

    기본 skeleton..

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+import os.path
+
+UPLOAD_FOLDER = "static/images"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'xlsx'])
 
 app = Flask(__name__)
 app.debug = True
 
+
+def allowed_file(file):
+    return '.' in file and file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/')
 def index():
-    return "templates/index.html"
+    return render_template("index.html")
 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+            print(file.filename);
+            # call the OCR function on it
+            # TODO: LPD
+
+
+            # TODO: SR
+            
+            
+            # TODO: OCR
+            
+
+            # extract the text and display it
+            return render_template('upload.html')
+    elif request.method == 'GET':
+        return render_template('upload.html')
 
 
 if __name__=='__main__':
