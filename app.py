a1ca34c8dd4ccad12a764ad01dbf3788e61fa5da
RaicLee <raiclee@gmail.com>
2021-04-30 13:54:51 +0900

    re

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request
-<<<<<<< HEAD
 import cv2 as cv
 import argparse
 import sys
@@ -11,24 +10,6 @@ from lpd import lpd
 UPLOAD_FOLDER = '/static/uploads/'
 RESULT_FOLDER = '/result/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-=======
-import os.path
-import cv2 as cv
-
-UPLOAD_FOLDER = "static/images"
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'xlsx'])
-
-
-"""
-#model configuration & weight file for License Plate Detection
-"""
-lpdcfg = "yolov4-ANPR.cfg"
-lpdweight = "yolov4-ANPR.weights"
-lpdnet = cv.dnn.readNetFromDarknet(lpdcfg, lpdweight)
-lpdnet.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-lpdnet.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
-
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
 
 app = Flask(__name__)
 
@@ -44,7 +25,6 @@ def allowed_file(file):
 
 
 @app.route('/')
-<<<<<<< HEAD
 def home_page():
     return render_template('index.html')
 
@@ -58,63 +38,23 @@ def upload_page():
         file = request.files['file']
         if file.filename == '':
             return render_template('upload.html', msg='No file selected')
-=======
-def index():
-    return render_template("index.html")
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_page():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return render_template('upload.html', msg='No file selected')
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            return render_template('upload.html', msg='No file selected')
 
         if file and allowed_file(file.filename):
             file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
             print(file.filename);
             # call the OCR function on it
-            # TODO: LPD
-
-
-            # TODO: SR
-            
-            
-            # TODO: OCR
-            
-
+            full_image,plates = lpd(file)
+            for i,pic in enumerate(plates):
+                cv.imwrite("result/"+full_image[:-4]+"_result"+str(i)+".jpg", pic.astype(np.uint8));
             # extract the text and display it
-            return render_template('upload.html')
-    elif request.method == 'GET':
-        return render_template('upload.html')
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
-
-        if file and allowed_file(file.filename):
-            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
-            print(file.filename);
-            # call the OCR function on it
-            license_plate_detect,location_list = lpd(file)
-            for i,pic in enumerate(location_list):
-                cv.imwrite("result/"+license_plate_detect[:-4]+"_result"+str(i)+".jpg", pic.astype(np.uint8));
-            # extract the text and display it
-            print(RESULT_FOLDER + license_plate_detect[:-4]+"_result0.jpg")
+            print(RESULT_FOLDER + full_image[:-4]+"_result0.jpg")
             return render_template('upload.html',
                                    msg='Successfully processed',
-                                   extracted_text=license_plate_detect,
-                                   img_src=UPLOAD_FOLDER + license_plate_detect,
-                                   lpd_src=RESULT_FOLDER + license_plate_detect[:-4]+"_result0.jpg")
+                                   extracted_text=full_image,
+                                   img_src=UPLOAD_FOLDER + full_image,
+                                   lpd_src=RESULT_FOLDER + full_image[:-4]+"_result0.jpg")
     elif request.method == 'GET':
         return render_template('upload.html')
 
-<<<<<<< HEAD
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-if __name__=='__main__':
-    app.run(host='0.0.0.0')
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
--- lpd.py
+++ lpd.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# This code is written at BigVision LLC. It is based on the OpenCV project. It is subject to the license terms in the LICENSE file found in this distribution and at http://opencv.org/license.html
-# Usage example:  python3 object_detection_yolo.py --video=run.mp4
-#                 python3 object_detection_yolo.py --image=bird.jpg
-=======
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
 import argparse
 from flask import Flask, render_template, request
 import cv2 as cv
@@ -11,7 +5,6 @@ import argparse
 import sys
 import numpy as np
 import os.path
-<<<<<<< HEAD
 UPLOAD_FOLDER = '/static/uploads/'
 def lpd(file):
     confThreshold = 0.4  #Confidence threshold
@@ -21,22 +14,10 @@ def lpd(file):
     inpHeight = 416 #608     #Height of network's input image
    
     classesFile = "DL/model/lpd/classes.names";
-=======
-UPLOAD_FOLDER = '/static/images/'
-confThreshold = 0.5  #Confidence threshold
-nmsThreshold = 0.4  #Non-maximum suppression threshold
-
-inpWidth = 416  #608     #Width of network's input image
-inpHeight = 416 #608     #Height of network's input image
-    
-    classesFile = "classes.names";
-
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
     classes = None
     with open(classesFile, 'rt') as f:
         classes = f.read().rstrip('\n').split('\n')
 
-<<<<<<< HEAD
     # Give the configuration and weight files for the model and load the network using them.
 
     modelConfiguration = "DL/model/lpd/yolov4-ANPR.cfg";
@@ -46,16 +27,6 @@ inpHeight = 416 #608     #Height of network's input image
     net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
-=======
-    lpdcfg = "yolov4-ANPR.cfg"
-    lpdweight = "yolov4-ANPR.weights"
-
-    lpdnet = cv.dnn.readNetFromDarknet(lpdcfg, lpdweight)
-    lpdnet.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-    lpdnet.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
-
-def lpd(file):
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
     # Get the names of the output layers
     def getOutputsNames(net):
         # Get the names of all the layers in the network
@@ -66,58 +37,31 @@ def lpd(file):
     # Draw the predicted bounding box
     def drawPred(classId, conf, left, top, right, bottom):
         # Draw a bounding box.
-<<<<<<< HEAD
-=======
-        #    cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
-        return frame[left:]
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
         cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
 
         label = '%.2f' % conf
 
-<<<<<<< HEAD
         if classes:
             assert(classId < len(classes))
             label = '%s:%s' % (classes[classId], label)
         
-=======
-        # Get the label for the class name and its confidence
-        if classes:
-            assert(classId < len(classes))
-            label = '%s:%s' % (classes[classId], label)
-
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
         #Display the label at the top of the bounding box
         labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
         top = max(top, labelSize[1])
         cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine), (0, 0, 255), cv.FILLED)
-<<<<<<< HEAD
         cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 2)
         crop_img = frame[top:bottom, left:right]
         return crop_img
-=======
-        #cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine),    (255, 255, 255), cv.FILLED)
-        cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 2)
-
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
     # Remove the bounding boxes with low confidence using non-maxima suppression
     def postprocess(frame, outs):
         frameHeight = frame.shape[0]
         frameWidth = frame.shape[1]
 
-<<<<<<< HEAD
         classIds = []
         confidences = []
         boxes = []
         # Scan through all the bounding boxes output from the network and keep only the
         # ones with high confidence scores. Assign the box's class label as the class with the highest score.
-=======
-        # Scan through all the bounding boxes output from the network and keep only the
-        # ones with high confidence scores. Assign the box's class label as the class with the highest score.
-        classIds = []
-        confidences = []
-        boxes = []
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
         for out in outs:
             print("out.shape : ", out.shape)
             for detection in out:
@@ -150,7 +94,6 @@ def lpd(file):
             top = box[1]
             width = box[2]
             height = box[3]
-<<<<<<< HEAD
             if (classIds[i] == 1):
                 #drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
                 License_list.append(drawPred(classIds[i], confidences[i], left, top, left + width, top + height))
@@ -159,9 +102,6 @@ def lpd(file):
 
 
 
-=======
-            drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
 
     # Process inputs
     f=os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename);
@@ -176,10 +116,7 @@ def lpd(file):
         outputFile = f[:-4]+'_yolo_out_py.jpg'
         out_image=file.filename[:-4]+'_yolo_out_py.jpg'
 
-<<<<<<< HEAD
     License_list= []
-=======
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
     while cv.waitKey(1) < 0:
 
         # get frame from the video
@@ -207,20 +144,10 @@ def lpd(file):
         # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
         t, _ = net.getPerfProfile()
         label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
-<<<<<<< HEAD
         cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-=======
-        #cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
 
         # Write the frame with the detection boxes
         if (f):
             cv.imwrite(outputFile, frame.astype(np.uint8));
-<<<<<<< HEAD
     print(len(License_list))
     return out_image,License_list;
-=======
-        else:
-            vid_writer.write(frame.astype(np.uint8))
-    return out_image;
->>>>>>> 614e37ee8633ff5a720e71a3a1ad0f4f1654b95a
