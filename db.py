6e7d4f0f38798dbece36cb5b1eb4953e5599381e
Clear-Top <6tank1004@gmail.com>
2021-05-11 06:00:24 +0900

    connect_db 함수정의 ->언제든 DB연결 호출가능

--- db.py
+++ db.py
@@ -1,8 +1,15 @@
 import pymysql
 import readExcel
 
+
+def connect_db():
+    conn = pymysql.connect(host='127.0.0.1', port=3307, user='root',
+                           passwd='1234', db='capstone', charset='utf8')
+    print('DB 연결성공')
+    return conn
+
+
 def select_all():
-    conn = pymysql.connect(host = '127.0.0.1', port = 3306, user = 'root', passwd = 'root', db = 'capstone', charset = 'utf8')
     try:
         with conn.cursor() as cursor:
             sql = "select * from class1"
@@ -11,30 +18,36 @@ def select_all():
             for row in rs:
                 print(row)
     finally:
-        conn.close()
+        return None
+
 
 def insert_test():
-    conn = pymysql.connect(host = '127.0.0.1', port = 3306, user = 'root', passwd = 'root', db = 'capstone', charset = 'utf8')
+    conn = pymysql.connect(host='127.0.0.1', port=3307, user='root',
+                           passwd='1234', db='capstone', charset='utf8')
     try:
         with conn.cursor() as curs:
-            i=2
+            i = 2
             while True:
                 list = []
-                for j in range(2,9):
-                    a = readExcel.read_excel(i,j)
+                for j in range(2, 9):
+                    a = readExcel.read_excel(i, j)
                     if a != 0:
                         list.append(a)
                     else:
-                        i=0
-                        break;
+                        i = 0
+                        break
                 if i == 0:
-                        break;
+                    break
                 else:
                     sql = 'insert into class1 values(%s,%s,%s,%s,%s,%s,%s)'
-                    curs.execute(sql, (list[0],list[1],list[2],list[3],list[4], list[5],list[6]))
+                    curs.execute(
+                        sql, (list[0], list[1], list[2], list[3], list[4], list[5], list[6]))
                     conn.commit()
                     i = i+1
     finally:
-        conn.close()
+        return None
 
-insert_test()
+# db 연결
+# connect_db()
+
+# insert_test()
