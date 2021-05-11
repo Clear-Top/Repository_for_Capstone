06aa72b1fa2feedfa2117cefdcd84348e92ffdc3
SejongEmo <cndwjd7102@naver.com>
2021-05-11 21:39:26 +0900

    엑셀 초기화 / 엑셀에 정보쓰기 추가

--- writeExcel.py
+++ writeExcel.py
@@ -1,28 +1,39 @@
 from openpyxl import Workbook
 
 
-def write_excel():
-    print('실행')
+def write_excel_prepare():
+    print('액셀작성실행')
     write_wb = Workbook()
 
-    # 이름이 있는 시트를 생성
-    #write_ws = write_wb.create_sheet('생성시트')
+    # 엑셀작성을 위한 정보 = {'워크시트','활성화키','입력개수'}
+    excel = {}
+    excel[0] = write_wb
+    excel[1] = write_wb.active
+    excel[2] = 1
+    return excel
 
-    # Sheet1에다 입력
-    write_ws = write_wb.active
-    write_ws['B1'] = '번호판'
-    write_ws['C1'] = '위치'
-    write_ws['D1'] = '날짜/시각'
-    write_ws['E1'] = '사진경로'
-    write_ws['F1'] = '사진이름'
-    write_ws['G1'] = '위도'
-    write_ws['H1'] = '경도'
+
+def write_excel_init(excel):
+    excel[1]['B1'] = '번호판'
+    excel[1]['C1'] = '위치'
+    excel[1]['D1'] = '날짜/시각'
+    excel[1]['E1'] = '사진경로'
+    excel[1]['F1'] = '사진이름'
+    excel[1]['G1'] = '위도'
+    excel[1]['H1'] = '경도'
+
+
+def write_excel(excel, carnum, gps, date, image_path, image_name, lalo):
+    # write_ws = write_wb.active
 
     # 행 단위로 추가
-    write_ws.append([1, 2, 3])
+    excel[1].append([excel[2], carnum, gps, date,
+                    image_path, image_name, lalo])
 
     # 셀 단위로 추가
-    write_wb.save('Excel/test2.xlsx')
+    excel[0].save('static/excel/download.xlsx')
+    excel[2] = excel[2] + 1
 
 
-write_excel()
+# excel = write_excel_prepare()
+# write_excel(excel, 1, 2, 3, 'static/upload/', 5, 6)
