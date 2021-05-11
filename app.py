834ec755ad43871ce44d5230409f65016bbc43b4
hyeounguk2 <jim20246@gmail.com>
2021-05-11 19:30:55 +0900

    /map 에서 엑셀업로드 하면 파일이름 넘겨주는 작업 추가

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ import argparse
 import sys
 import numpy as np
 import os.path
-from lpd import lpd
+import db
+from lpd import lpd 
 from lpr import lpr
-import searchCar
 UPLOAD_FOLDER = '/static/uploads/'
 RESULT_FOLDER = '/result/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-ALLOWED_EXCEL = set(['xlsx', 'xls'])
+ALLOWED_EXCEL = set(['xlsx','xls'])
 app = Flask(__name__)
-
 
 def allowed_excel(filename):
     return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXCEL
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXCEL
 
 
 def allowed_file(filename):
@@ -28,29 +27,26 @@ def allowed_file(filename):
 def allowed_file(file):
     return '.' in file and file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-
 @app.route('/map')
 def map():
-    carnums = searchCar.read_carnum()
-    return render_template('mapPage.html', carnum=carnums)
-
-
+    return render_template('mapPage.html')
 @app.route('/xlupload', methods=['GET', 'POST'])
 def upload_excel():
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_excel(file.filename):
-            file.save(os.path.join(os.getcwd()+'/static/excel/', file.filename))
-            return render_template('mapPage.html')
+        if file and allowed_excel(file.filename) :
+            file.save(os.path.join(os.getcwd()+'/static/excel/',file.filename))
+            #엑셀업로드+db반영
+            conn=db.connect_db()
+            db.insert_test(file.filename,conn)
+            conn.close()
+            return 'success'
     elif request.method == 'GET':
         return render_template('mapPage.html')
-
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():
     if request.method == 'POST':
@@ -64,25 +60,23 @@ def upload_page():
         if file and allowed_file(file.filename):
             file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
             print(file.filename)
-
-            full_image, plates = lpd(file)
+            
+            full_image,plates = lpd(file)
 
             plate_num = []
-            plate_prob= []
             print("[SYS] lpr ")
-            for i, pic in enumerate(plates):
+            for i,pic in enumerate(plates):
                 if pic is not None:
                     try:
                         print("[",i,"]",pic.shape)
-                        #pic = cv.resize( pic, None, fx = 3, fy = 3, interpolation = cv.INTER_CUBIC)
-                        lp,prob = lpr(pic)
-                        plate_num.append(lp)
-                        plate_prob.append(float(prob[0][0]))
+                        pic = cv.resize( pic, None, fx = 3, fy = 3, interpolation = cv.INTER_CUBIC)
+                        plate_num.append(lpr(pic))
+
                         cv.imwrite("result/"+full_image[:-4]+"_result"+str(i)+".jpg", pic.astype(np.uint8))
                     except:
                         continue
-            for i in range(len(plate_num)):
-                print("[PROB ",i,"]", plate_num[i], plate_prob[i])
+            for i in plate_num:
+                print(i)
             
             return render_template('upload.html',
                                    msg='Successfully processed',
@@ -91,7 +85,6 @@ def upload_page():
                                    lpd_src=RESULT_FOLDER + full_image[:-4]+"_result0.jpg")
     elif request.method == 'GET':
         return render_template('upload.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
