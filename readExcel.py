81193e024c6c7067516e0ab625c752339eee685d
hyeounguk2 <jim20246@gmail.com>
2021-05-11 19:29:49 +0900

    readExcel 엑셀파일이름으로 접근하게 함

--- readExcel.py
+++ readExcel.py
@@ -1,11 +1,11 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
+import app
 
-
-def read_excel(i,j):
-
+def read_excel(i,j,excel_name):
+    #excel_name은 엑셀파일 이름을 가져온 것이다.
     # data_only=Ture로 해줘야 수식이 아닌 값으로 받아온다.
-    load_wb = load_workbook("static\excel/test1.xlsx", data_only=True)
+    load_wb = load_workbook("static/excel/"+ excel_name, data_only=True)
     # 시트 이름으로 불러오기
     load_ws = load_wb['Sheet']
 
