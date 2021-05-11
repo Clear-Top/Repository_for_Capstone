94654a61273cba510897aae8e810d375ac20c1b9
SejongEmo <cndwjd7102@naver.com>
2021-05-11 21:38:14 +0900

    시간, 위경도 EXIF정보 반환

--- extractExif.py
+++ extractExif.py
@@ -2,16 +2,40 @@ from PIL import Image
 from PIL.ExifTags import GPSTAGS
 from PIL.ExifTags import TAGS
 
-def get_labeled_exif(exif):
-    labeled = {}
-    for (key, val) in exif.items():
-        labeled[TAGS.get(key)] = val
-    return labeled
+# import pandas as pd
+# import requests
+# import sys
+# import json
+# import datetime
 
+# import requests
+# from urllib.parse import urlparse
+# def get_labeled_exif(exif):
+#     labeled = {}
+#     for (key, val) in exif.items():
+#         labeled[TAGS.get(key)] = val
+#     return labeled
+# APP_KEY = ''
+# url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=127.1086228&y=37.4012191"
+
+# 날짜정보추출
+
+
+def get_exif_time(filename):
+    info = get_exif("static/uploads/"+filename)
+    if info[36867] is not None:
+        make_time = info[36867]
+        return make_time
+    else:
+        return None
+
+
+# 인자 : 이미지경로 , 반환값 : 모든메타정보
 def get_exif(filename):
     image = Image.open(filename)
     image.verify()
     return image._getexif()
+
 
 def get_geotagging(exif):
     if not exif:
@@ -29,6 +53,7 @@ def get_geotagging(exif):
 
     return geotagging
 
+
 def get_decimal_from_dms(dms, ref):
     degrees = dms[0]
     minutes = dms[1] / 60.0
@@ -40,10 +65,36 @@ def get_decimal_from_dms(dms, ref):
         seconds = -seconds
     return round(degrees + minutes + seconds, 5)
 
+
 def get_coordinates(geotags):
-    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+    lat = get_decimal_from_dms(
+        geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
 
-    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+    lon = get_decimal_from_dms(
+        geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
 
-    return (lat,lon)
+    return (lat, lon)
 
+
+# print(get_exif_time())
+# print(get_coordinates(get_geotagging(get_exif("static/images/test.jpg"))))
+
+
+# # 지오코딩
+# address = '서울 종로구 평동 233 3106호'
+# url = 'https://dapi.kakao.com/v2/local/search/address.json?&query=' + address
+# result = requests.get(urlparse(url).geturl(), headers={
+#                       'Authorization': 'KakaoAK 99dc47651e0b80e1160d836211f9c8d9'}).json()
+# match_first = result['documents'][0]['address']
+# lat = float(match_first['y'])
+# lng = float(match_first['x'])
+# print(lat, lng)  # 위도(lat) 경도(long)
+
+
+# # 지오코딩 2
+# url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=127.1086228&y=37.4012191"
+# headers = {"Authorization": "KakaoAK 99dc47651e0b80e1160d836211f9c8d9"}
+# api_test = requests.get(url, headers=headers)
+# url_text = json.loads(api_test.text)
+# url_text
+# url_text['documents'][0]['address_name']
