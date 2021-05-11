3b3ea18b49936c230d8355ef3ee429e23c1a5ed9
RaicLee <raiclee@gmail.com>
2021-05-11 23:37:22 +0900

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ UPLOAD_FOLDER = '/static/uploads/'
 RESULT_FOLDER = '/result/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 ALLOWED_EXCEL = set(['xlsx','xls'])
+kor_dict   = ["가", "나", "다", "라", "마", "거", "너", "더", "러",
+              "머", "버", "서", "어", "저", "고", "노", "도", "로",
+              "모", "보", "소", "오", "조", "구", "누", "두", "루",
+              "무", "부", "수", "우", "주", "허", "하", "호"]
+num_dict   = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 app = Flask(__name__)
+
+
+
+
 
 def allowed_excel(filename):
     return '.' in filename and \
@@ -64,22 +73,30 @@ def upload_page():
             full_image,plates = lpd(file)
 
             plate_num = []
+            plate_prob = []
             print("[SYS] lpr ")
             for i,pic in enumerate(plates):
                 if pic is not None:
                     try:
                         print("[",i,"]",pic.shape)
-                        #pic = cv.resize( pic, None, fx = 3, fy = 3, interpolation = cv.INTER_CUBIC)
                         lp,prob = lpr(pic)
-                        plate_num.append(lp)
-                        print(type(lp),lp.shape)
+                        plate_num.append(lp[0])
                         plate_prob.append(float(prob[0][0]))
                         cv.imwrite("result/"+full_image[:-4]+"_result"+str(i)+".jpg", pic.astype(np.uint8))
                     except:
                         continue
-            for i in plate_num:
-                print(i)
-            
+            for i in range(len(plate_num)):
+                try:
+                    print("[PROB ",i,"]", plate_num[i],len(plate_num[i]))
+                    if plate_num[i][-5] not in kor_dict:
+                        print("wrong assumption")
+                    else:
+                        if (len(plate_num[i]) == 6 or len(plate_num[i]) == 7):
+                            print("right length")
+                        else:
+                            print("wrong length")
+                except:
+                    print(i,"index out of range")
             return render_template('upload.html',
                                    msg='Successfully processed',
                                    extracted_text=full_image,
