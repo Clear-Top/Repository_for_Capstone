5de882eef0b7b41e82824c4859e7b3ea7f20584b
Clear-Top <6tank1004@gmail.com>
2021-05-30 01:51:12 +0900

    서비스소개 - 맵조회 / 로고디자인 / 사이드바 JS

--- static/js/sideBar.js
+++ static/js/sideBar.js
@@ -6,17 +6,22 @@ $(document).ready(function () {
     })
     $('#closeNav').click(function () {
         // alert('사이드바를 닫습니다.');
-        $('#nav-sub').fadeOut(500);
+        $('#nav-sub').fadeOut(300);
     })
 });
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+    document.getElementById("main").style.opacity = 0.2;
+    var p = document.getElementsByClassName('logo');
+    p[0].style.opacity = 0.2;
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.opacity = 1.0;
     document.body.style.backgroundColor = "white";
+    var p = document.getElementsByClassName('logo');
+    p[0].style.opacity = 1.0;
+
 }
